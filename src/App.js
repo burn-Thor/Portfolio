cd61dec4822d29d33f131eaaad04bcd68f783937
burn-Thor <eothorburn@gmail.com>
2022-08-24 11:18:27 +0100

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-Portfoliooo
+    <div className="app">
+    <BrowserRouter>
+      <Home/>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className="home">
+        <h1>Home Page</h1>
+    </div>
+
+    </>
+  )
+}
+
+export default Home
